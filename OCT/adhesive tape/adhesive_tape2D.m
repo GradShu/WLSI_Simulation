5f884056d872037a82e21eec17395a20850506ad
舒奥 <shuao.hqu@gmail.com>
2024-01-10 23:46:20 +0800

    修改了生成的OCT图像坐标轴。

--- OCT/adhesive tape/adhesive_tape2D.m
+++ OCT/adhesive tape/adhesive_tape2D.m
@@ -1,5 +1,5 @@
 close all;clc;clear;
-handle  = OCTFileOpen('Default_0186_Mode2D.oct');
+handle  = OCTFileOpen('Default_0193_Mode2D.oct');
 disp( OCTFileGetProperty(handle, 'AcquisitionMode') );
 disp( OCTFileGetProperty(handle, 'RefractiveIndex') );
 disp( OCTFileGetProperty(handle, 'Comment') );
@@ -10,10 +10,9 @@ disp( OCTFileGetProperty(handle, 'ExperimentNumber') );
 NrRawData = OCTFileGetNrRawData(handle);
 [RawData, Spectrum] = OCTFileGetRawData(handle, 0);
 % plot(1:2048,RawData(:,1));
-
 load('Wavelength.mat','Wavelength');
 [row, column] = size(RawData);
-Nifft = 2^11;
+Nifft = 2^15;
 gray_value = zeros(Nifft/2,column);
 for i = 1:column
    [z,intensity_ifft] = A_scan(Wavelength,Spectrum,RawData(:,i),Nifft);
@@ -22,16 +21,17 @@ for i = 1:column
 end
 % image(gray_value);
 % I = mat2gray(gray_value);
-imshow(gray_value,[]);
-imwrite(uint8(gray_value), 'matrix.tif' );
+imshow(gray_value,[],'XData', [0, 4.5], 'YData', [0, 2.8],'InitialMagnification','fit');
+set(gca, 'YDir', 'reverse');
+%将小Tick打开
+set(gca,'XMinorTick','on')
+set(gca,'YMinorTick','on')
+axis on;
+axis image
 
 %二维层析图像绘制
-
 function [z,intensity_ifft] = A_scan(Wavelength,Spectrum,RawData,Nifft)
     Wavelength = Wavelength';
-%     plot(1:length(Wavelength),Wavelength);
-%     plot(Wavelength,Spectrum);
-%     plot(Wavelength,RawData(:,1));
     spectrum = RawData(:,1);
     %消除直流项对ifft的影响
     spectrum = spectrum - Spectrum;
@@ -49,12 +49,9 @@ function [z,intensity_ifft] = A_scan(Wavelength,Spectrum,RawData,Nifft)
     sigma_inter = linspace(sigma(1),sigma(end),length(sigma));
     %光谱强度三次样条插值
     intensity_inter = spline(sigma,intensity,sigma_inter);
-%     plot(sigma_inter,intensity_inter);
     %干涉光谱整形（采用汉宁窗）
     intensity_shaping = intensity_inter.* (hann(length(intensity_inter)))';
-%     plot(sigma_inter,intensity_shaping);
     %对光谱进行傅里叶逆变换
-%     Nifft = 2^15;
     interference_signal = ifft(intensity_shaping,Nifft);
 %     interference_signal = ifftshift(interference_signal);
     deltaSigma = (sigma_inter(end)-sigma_inter(1))/(length(sigma_inter)-1);
